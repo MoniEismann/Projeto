70c73c67d191133bd9c185d32d321c3dcef6890f
Mônica Eismann <monica.eismann@gmail.com>
2024-01-31 13:39:51 -0300

    projeto inicial

---
+++ js-curso-2-aula_3/ex.js
@@ -0,0 +1,13 @@
+let listaGenerica = [];
+
+let linguagensDeProgramação = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+
+linguagensDeProgramação.push('Java', 'Ruby', 'GoLang');
+
+let listaDeNomes = ['Chila', 'Ramon', 'Kashi'];
+console.log (listaDeNomes [0]);
+console.log (listaDeNomes [1]);
+console.log (listaDeNomes[listaDeNomes.length - 1]);
+
+
+
